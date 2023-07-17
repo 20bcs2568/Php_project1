dcb94f9a1098d3e6540dad058005892224919f5e
Harshit <bansalharshit31830@gmail.com>
2023-07-17 23:51:13 +0530

    commit files

---
+++ userinformation.php
@@ -0,0 +1,17 @@
+<?php
+
+$connection = mysqli_connect('localhost', 'root');
+
+mysqli_select_db($connection,"website");
+
+$user = $_POST['user'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+$query = "INSERT INTO `userinfodata`(`user`,`email`,`message`) VALUES ('$user','$email','$message') ";
+
+mysqli_query($connection,$query);
+
+echo "MESSAGE IS SENT";
+
+?>
